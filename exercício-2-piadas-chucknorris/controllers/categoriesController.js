bab8ee20d53f85936887baa2a8af3a0016add58f
Viegas <mvrdgs@gmail.com>
2021-10-21 11:56:23 -0300

    criado controller de categoria

---
+++ exercício-2-piadas-chucknorris/controllers/categoriesController.js
@@ -0,0 +1,7 @@
+const categoriesModel = require('../models/categories');
+
+const listCategories = async (req, res) => {
+  const categories = await categoriesModel.getCategoriesList();
+
+  return res.status(200).render('categories', { categories });
+};
